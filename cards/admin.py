2a7da63e94d8a22954ed215f5d1787e11b4e6682
NIbrahimB <nibrahimb1993@gmail.com>
2018-12-20 11:02:33 +0300

    update model and admin.

--- cards/admin.py
+++ cards/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
 from django.utils.translation import gettext_lazy as _
 
 from cards.models import Card
+
+USER_MODEL = get_user_model()
 
 
 @admin.register(Card)
@@ -14,6 +18,11 @@ class CardAdmin(admin.ModelAdmin):
         "sex",
         "_action",
     ]
+    readonly_fields = [
+        "created_by",
+        "created_at",
+        "updated_at",
+    ]
 
     def _action(self, card):
         return '<a target="_blank" href="/print_card/{}/">{}</a> / <a href="/download_card/{}/">{}</a>'.format(
@@ -25,3 +34,31 @@ class CardAdmin(admin.ModelAdmin):
 
     _action.short_description = _('Action')
     _action.allow_tags = True
+
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        if not request.user.is_superuser:
+            queryset = queryset.filter(created_by=request.user)
+        return queryset
+
+    def save_model(self, request, obj, form, change):
+        if not obj.pk:
+            obj.created_by = request.user
+        if not obj.expire_at:
+            obj.expire_at = obj.started_at.replace(year=obj.started_at.year + 1)
+        return super().save_model(request, obj, form, change)
+
+
+admin.site.unregister(USER_MODEL)
+
+
+@admin.register(USER_MODEL)
+class CustomUserAdmin(UserAdmin):
+    actions = ['reset_password']
+
+    def reset_password(self, request, queryset):
+        for user in queryset:
+            user.set_password("123")
+            user.save()
+
+    reset_password.short_description = _("Reset password")
--- cards/models.py
+++ cards/models.py
@@ -1,10 +1,19 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 from cards import SEX_TYPES_CHOICES
 
+USER_MODEL = get_user_model()
+
 
 class Card(models.Model):
+    created_by = models.ForeignKey(USER_MODEL,
+                                   null=True,
+                                   blank=True,
+                                   on_delete=models.CASCADE,
+                                   help_text=_("The user who create the card"),
+                                   verbose_name=_("Create by"))
     full_name = models.CharField(max_length=100, help_text=_("Full name for card holder"), verbose_name=_("Full Name"))
     phone_number = models.CharField(max_length=15, help_text=_("Phone number"), verbose_name=_("Phone number"))
     id_number = models.CharField(max_length=10, help_text=_("ID number"), verbose_name=_("ID number"))
@@ -13,6 +22,12 @@ class Card(models.Model):
                            verbose_name=_("Sex type"))
     created_at = models.DateTimeField(auto_now_add=True, help_text=_("Created at"), verbose_name=_("Created at"))
     updated_at = models.DateTimeField(auto_now=True, help_text=_("Updated at"), verbose_name=_("Updated at"))
+    started_at = models.DateField(help_text=_("The date of start"),
+                                  verbose_name=_("Started at"))
+    expire_at = models.DateField(help_text=_("The date of expire"),
+                                 verbose_name=_("Expire at"),
+                                 blank=True,
+                                 null=True)
 
     def __str__(self):
         return "{} - {} - {}".format(_("Card"), self.id, self.full_name)
